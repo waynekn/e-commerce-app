fed8bc80873d020a8273c58c0df5f31c9a7c3d5f
Wayne Kinyanjui <waynekn24@gmail.com>
2024-07-17 01:01:01 +0300

    typo fix

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,25 +1,14 @@
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { createAction } from "../../utils/reducer/reducer.utils";
-import { CATEGORIES_ACTION_TYPE } from "./category.types";
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export const fetchCategoriesStart = () =>
-  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
 
 export const fetchCategoriesSuccess = (categoriesArray) =>
   createAction(
-    CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
     categoriesArray
   );
 
 export const fetchCategoriesFailed = (error) =>
-  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try {
-    const categoriesArray = await getCategoriesAndDocuments();
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-};
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
